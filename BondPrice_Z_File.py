e89bb95eaa0c13466103d7ae8908b590ee2eea2c
Lindaaa12345 <166074924+Lindaaa12345@users.noreply.github.com>
2024-04-11 19:32:38 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,13 @@
+def getBondPrice_Z(face, couponRate, times, yc):
+    coupon_payment = face * couponRate
+    bondPrice = 0
 
+    for time, y in zip(times, yc):
+        cash_flow = coupon_payment + (face if time == times[-1] else 0)
+        pv = cash_flow / ((1 + y)**time)
+        bondPrice += pv
+
+    return(bondPrice)
 
 def getBondPrice_Z(face, couponRate, times, yc):
     return(1996533)
