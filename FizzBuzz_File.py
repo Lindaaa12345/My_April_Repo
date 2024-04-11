4372487da6a5fcb381eabafbfbb0f00ef54415f7
Lindaaa12345 <166074924+Lindaaa12345@users.noreply.github.com>
2024-04-11 19:34:33 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,3 +1,15 @@
+def FizzBuzz(start, finish):
+    outlist = []
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            outlist.append("fizzbuzz")
+        elif i % 3 == 0:
+            outlist.append("fizz")
+        elif i % 5 == 0:
+            outlist.append("buzz")
+        else:
+            outlist.append(str(i))
+    return(outlist)
 
 
 def FizzBuzz(start, finish):
