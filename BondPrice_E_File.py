62fec44ad169867c865a60468f1c798bbb582606
Lindaaa12345 <166074924+Lindaaa12345@users.noreply.github.com>
2024-04-11 19:30:37 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,13 @@
+def getBondPrice_E(face, couponRate, m, yc):
+    coupon_payment = face * couponRate
+    bondPrice = 0
 
+    for t, y in enumerate(yc, start=1):
+        cash_flow = coupon_payment + (face if t == m else 0)
+        pv = cash_flow / ((1 + y)**t)
+        bondPrice += pv
+
+    return(bondPrice)
 
 def getBondPrice_E(face, couponRate, m, yc):
     return(2098949)
