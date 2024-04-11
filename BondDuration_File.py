69d3b386b7303d480bfb06aab2bb884c8126e600
Lindaaa12345 <166074924+Lindaaa12345@users.noreply.github.com>
2024-04-11 19:28:34 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,16 @@
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    coupon_payment = face * couponRate / ppy
+    sum_pvcf = 0
+    sum_weighted_pvcf = 0
+    for t in range(1, m+1):
+        cash_flow = coupon_payment + (face if t == m else 0)
+        pvcf = cash_flow / ((1 + y)**t)
+        sum_pvcf += pvcf
+        sum_weighted_pvcf += pvcf * t
 
+    bondDuration = sum_weighted_pvcf / sum_pvcf
+    
+    return(bondDuration)
+    
 def getBondDuration(y, face, couponRate, m, ppy = 1):
     return(8.51)
